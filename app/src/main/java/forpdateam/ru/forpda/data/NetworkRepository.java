d4dd16688281e90c612d7dfef1310b708b4411fd
isanechek <neizvestniytip@gmail.com>
2016-11-10 07:41:23 +0400

    Я думаю так будет лучше. Не будет каждый весь стек инстансов создавать или перепроверять.

--- app/src/main/java/forpdateam/ru/forpda/data/NetworkRepository.java
+++ app/src/main/java/forpdateam/ru/forpda/data/NetworkRepository.java
@@ -5,8 +5,8 @@ import android.support.annotation.Nullable;
 
 import java.util.ArrayList;
 
+import forpdateam.ru.forpda.api.Api;
 import forpdateam.ru.forpda.api.profile.models.ProfileModel;
-import forpdateam.ru.forpda.api.qms.Qms;
 import forpdateam.ru.forpda.api.qms.models.QmsChatModel;
 import forpdateam.ru.forpda.api.qms.models.QmsContact;
 import forpdateam.ru.forpda.api.qms.models.QmsThemes;
@@ -25,19 +25,14 @@ public class NetworkRepository implements DataSource.NetworkDataSource {
     @Nullable
     private static NetworkRepository INSTANCE = null;
 
-    @Nullable
-    private final Qms qms;
-
-    private NetworkRepository() {
-        qms = new Qms();
-
-    }
-
     public static NetworkRepository getInstance() {
         if (INSTANCE == null) {
             INSTANCE = new NetworkRepository();
         }
         return INSTANCE;
+    }
+
+    private NetworkRepository() {
     }
 
     public static void destroyInstance() {
@@ -97,8 +92,7 @@ public class NetworkRepository implements DataSource.NetworkDataSource {
     @Override
     public Observable<String> deleteDialog(@NonNull String mid) {
         checkNotNull(mid, "Delete Dialog! Mid " + CNBN);
-        checkNotNull(qms, "Delete Dialog! Qms Instance "+ CNBN);
-        return qms.deleteDialog(mid);
+        return Api.Qms().deleteDialog(mid);
     }
 
     @Override
@@ -106,35 +100,30 @@ public class NetworkRepository implements DataSource.NetworkDataSource {
         checkNotNull(nick, "Send New Theme! Nick " + CNBN);
         checkNotNull(title, "Send New Theme! Title " + CNBN);
         checkNotNull(mess, "Send New Theme! Mess " + CNBN);
-        checkNotNull(qms, "Send New Theme! Qms Instance "+ CNBN);
-        return qms.sendNewTheme(nick, title, mess);
+        return Api.Qms().sendNewTheme(nick, title, mess);
     }
 
     @Override
     public Observable<String[]> search(@NonNull String nick) {
         checkNotNull(nick, "Search! Nick " + CNBN);
-        checkNotNull(qms, "Search! Qms Instance " + CNBN);
-        return qms.search(nick);
+        return Api.Qms().search(nick);
     }
 
     @Override
     public Observable<QmsChatModel> getChat(@NonNull String userId, @NonNull String themeId) {
         checkNotNull(userId, "Get Chat! User Id " + CNBN);
         checkNotNull(themeId, "Get Chat! Theme Id " + CNBN);
-        checkNotNull(qms, "Get Chat! Qms Instance " + CNBN);
-        return qms.getChat(userId, themeId);
+        return Api.Qms().getChat(userId, themeId);
     }
 
     @Override
     public Observable<QmsThemes> getThemesList(@NonNull String id) {
         checkNotNull(id, "Get Theme List! Id " + CNBN);
-        checkNotNull(qms, "Get Theme List! QMS Instance " + CNBN);
-        return qms.getThemesList(id);
+        return Api.Qms().getThemesList(id);
     }
 
     @Override
     public Observable<ArrayList<QmsContact>> getContactList() {
-        checkNotNull(qms, "get Contact List! Qms Instance " + CNBN);
-        return qms.getContactList();
+        return Api.Qms().getContactList();
     }
 }
