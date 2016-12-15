0de47b3a938b296361d6fe231cd87d74927fd401
iSanechek <neizvestniytip@gmail.com>
2016-12-15 06:17:58 +0400

    Refactoring update dependencies

--- app/src/main/java/forpdateam/ru/forpda/api/newslist/NewsList.java
+++ app/src/main/java/forpdateam/ru/forpda/api/newslist/NewsList.java
@@ -4,14 +4,62 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import forpdateam.ru.forpda.api.newslist.models.NewsItem;
 import forpdateam.ru.forpda.client.Client;
+import forpdateam.ru.forpda.fragments.news.NewsModel;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import static forpdateam.ru.forpda.Constants.NEWS_CATEGORY_ALL;
+import static forpdateam.ru.forpda.Constants.NEWS_CATEGORY_ARTICLES;
+import static forpdateam.ru.forpda.Constants.NEWS_CATEGORY_GAMES;
+import static forpdateam.ru.forpda.Constants.NEWS_CATEGORY_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_CATEGORY_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_ACCESSORIES_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_ACOUSTICS_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_ANDROID_GAME;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_ANDROID_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_DEVSTORY_GAMES;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_DEVSTORY_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_HOW_TO_ANDROID;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_HOW_TO_INTERVIEW;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_HOW_TO_IOS;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_HOW_TO_WP;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_IOS_GAME;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_IOS_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_NOTEBOOKS_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_SMARTPHONES_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_SMART_WATCH_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_TABLETS_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_WP7_GAME;
+import static forpdateam.ru.forpda.Constants.NEWS_SUBCATEGORY_WP7_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_ACCESSORIES_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_ACOUSTICS_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_ALL;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_ANDROID_GAME;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_ANDROID_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_ARTICLES;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_DEVSTORY_GAMES;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_DEVSTORY_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_GAMES;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_HOW_TO_ANDROID;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_HOW_TO_INTERVIEW;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_HOW_TO_IOS;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_HOW_TO_WP;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_IOS_GAME;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_IOS_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_NOTEBOOKS_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_SMARTPHONES_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_SMART_WATCH_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_SOFTWARE;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_TABLETS_REVIEWS;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_WP7_GAME;
+import static forpdateam.ru.forpda.Constants.NEWS_URL_WP7_SOFTWARE;
 
 /**
  * Created by radiationx on 31.07.16.
@@ -61,5 +109,98 @@ public class NewsList {
                 }
             }
         });
+    }
+
+
+
+    /*Experimental*/
+    public static List<NewsModel> getNewsItems(@NonNull String category, int number) {
+        List<NewsModel> cache = new ArrayList<>();
+        String url = getUrlCategory(category);
+        if (number >= 2) {
+            url = url + "page/" + number + "/";
+        }
+        String response = null;
+        try {
+            response = Client.getInstance().get(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (response == null) return new ArrayList<>();
+
+        Matcher matcher = pattern.matcher(response);
+        while (matcher.find()) {
+            NewsModel model = new NewsModel();
+            model.setLink(matcher.group(1));
+            model.setCategory(category);
+            model.setImgLink(matcher.group(3));
+            model.setTitle(matcher.group(2));
+            model.setCommentsCount(matcher.group(4));
+            model.setDate(matcher.group(5));
+            model.setAuthor(matcher.group(6));
+            model.setDescription(matcher.group(7));
+            model.setRead(false);
+            model.setOffline(false);
+            cache.add(model);
+
+        }
+        return cache;
+    }
+
+    private static String getUrlCategory(@NonNull String category) {
+        switch (category) {
+            case NEWS_CATEGORY_ALL:
+                return NEWS_URL_ALL;
+            case NEWS_CATEGORY_ARTICLES:
+                return NEWS_URL_ARTICLES;
+            case NEWS_CATEGORY_REVIEWS:
+                return NEWS_URL_REVIEWS;
+            case NEWS_CATEGORY_SOFTWARE:
+                return NEWS_URL_SOFTWARE;
+            case NEWS_CATEGORY_GAMES:
+                return NEWS_URL_GAMES;
+            case NEWS_SUBCATEGORY_DEVSTORY_GAMES:
+                return NEWS_URL_DEVSTORY_GAMES;
+            case NEWS_SUBCATEGORY_WP7_GAME:
+                return NEWS_URL_WP7_GAME;
+            case NEWS_SUBCATEGORY_IOS_GAME:
+                return NEWS_URL_IOS_GAME;
+            case NEWS_SUBCATEGORY_ANDROID_GAME:
+                return NEWS_URL_ANDROID_GAME;
+            case NEWS_SUBCATEGORY_DEVSTORY_SOFTWARE:
+                return NEWS_URL_DEVSTORY_SOFTWARE;
+            case NEWS_SUBCATEGORY_WP7_SOFTWARE:
+                return NEWS_URL_WP7_SOFTWARE;
+            case NEWS_SUBCATEGORY_IOS_SOFTWARE:
+                return NEWS_URL_IOS_SOFTWARE;
+            case NEWS_SUBCATEGORY_ANDROID_SOFTWARE:
+                return NEWS_URL_ANDROID_SOFTWARE;
+            case NEWS_SUBCATEGORY_SMARTPHONES_REVIEWS:
+                return NEWS_URL_SMARTPHONES_REVIEWS;
+            case NEWS_SUBCATEGORY_TABLETS_REVIEWS:
+                return NEWS_URL_TABLETS_REVIEWS;
+            case NEWS_SUBCATEGORY_SMART_WATCH_REVIEWS:
+                return NEWS_URL_SMART_WATCH_REVIEWS;
+            case NEWS_SUBCATEGORY_ACCESSORIES_REVIEWS:
+                return NEWS_URL_ACCESSORIES_REVIEWS;
+            case NEWS_SUBCATEGORY_NOTEBOOKS_REVIEWS:
+                return NEWS_URL_NOTEBOOKS_REVIEWS;
+            case NEWS_SUBCATEGORY_ACOUSTICS_REVIEWS:
+                return NEWS_URL_ACOUSTICS_REVIEWS;
+            case NEWS_SUBCATEGORY_HOW_TO_ANDROID:
+                return NEWS_URL_HOW_TO_ANDROID;
+            case NEWS_SUBCATEGORY_HOW_TO_IOS:
+                return NEWS_URL_HOW_TO_IOS;
+            case NEWS_SUBCATEGORY_HOW_TO_WP:
+                return NEWS_URL_HOW_TO_WP;
+            case NEWS_SUBCATEGORY_HOW_TO_INTERVIEW:
+                return NEWS_URL_HOW_TO_INTERVIEW;
+        }
+        return NEWS_URL_ALL;
+    }
+
+    public static String getDetailsNewsItem(@NonNull String url) throws Exception {
+        return Client.getInstance().get(url);
     }
 }
