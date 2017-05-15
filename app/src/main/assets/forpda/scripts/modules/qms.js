75f5f7c40b598f2d2b60682175232e1afaeec3d9
RadiationX <radiationx@yandex.ru>
2017-05-15 23:27:07 +0300

    new struct web
    Former-commit-id: 08411e107fb8afd5b9ae7bc2e3a30dc4559b8e22

---
+++ app/src/main/assets/forpda/scripts/modules/qms.js
@@ -0,0 +1,55 @@
+var listElem;
+
+window.addEventListener("DOMContentLoaded", function (e) {
+    listElem = document.querySelector(".mess_list");
+    setTimeout(function () {
+        getLastMess().scrollIntoView();
+    }, 10);
+});
+
+window.addEventListener("load", function (e) {
+    getLastMess().scrollIntoView();
+});
+
+var lastMessRequestTS = new Date().getTime();
+
+window.addEventListener("scroll", function (e) {
+    var date = new Date();
+    if (window.pageYOffset == 0 /*|| window.pageYOffset <= 48*/ && (date.getTime() - lastMessRequestTS >= 500)) {
+        lastMessRequestTS = date.getTime();
+        //addTopPost();
+        IChat.showMoreMess();
+    }
+});
+
+function getLastMess() {
+    var messages = listElem.querySelectorAll(".mess_container");
+    return messages[messages.length - 1];
+}
+
+function showMoreMess(listSrc) {
+    var lastHeight = listElem.offsetHeight;
+    listElem.insertAdjacentHTML("afterbegin", listSrc);
+    window.scrollBy(0, listElem.offsetHeight - lastHeight);
+}
+
+function showNewMess(listSrc, withScroll) {
+    listElem.insertAdjacentHTML("beforeend", listSrc);
+    if (withScroll) {
+        getLastMess().scrollIntoView();
+    }
+}
+
+function addTopPost() {
+    var listSrc = ""
+    for (var i = 0; i < 30; i++) {
+        listSrc += '<div class="mess_container our"><div class="mess"><div class="content">cheburekkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</div></div><div class="date"><span>04:20</span></div></div>';
+    }
+    listSrc += "<hr><hr>";
+    showMoreMess(listSrc);
+}
+
+function addPost() {
+    var listSrc = '<div class="mess_container our"><div class="mess"><div class="content">cheburekkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</div></div><div class="date"><span>04:20</span></div></div>';
+    showNewMess(listSrc, true);
+}
