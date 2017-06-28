6e66c99eff8b45943a7e1f13574e13f6c5d40a5e
RadiationX <radiationx@yandex.ru>
2017-06-28 15:25:59 +0300

    webview font size preference
    Former-commit-id: e744d7ecef70eecf4badc20bd344e6c08083ff82

--- app/src/main/java/forpdateam/ru/forpda/settings/SettingsFragment.java
+++ app/src/main/java/forpdateam/ru/forpda/settings/SettingsFragment.java
@@ -1,8 +1,13 @@
 package forpdateam.ru.forpda.settings;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import forpdateam.ru.forpda.App;
@@ -22,7 +27,7 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
-        if(ClientHelper.getAuthState()){
+        if (ClientHelper.getAuthState()) {
             findPreference("auth.action.logout").setOnPreferenceClickListener(preference -> {
                 new AlertDialog.Builder(getActivity())
                         .setMessage("Вы уверены, что хотите выйти из аккаунта?")
@@ -41,10 +46,51 @@ public class SettingsFragment extends PreferenceFragment {
                         .show();
                 return false;
             });
-        }else {
+        } else {
             findPreference("auth.action.logout").setEnabled(false);
         }
 
         findPreference("about.application").setSummary("Версия " + BuildConfig.VERSION_NAME);
+
+        findPreference(Preferences.Main.WEBVIEW_FONT_SIZE).setOnPreferenceClickListener(preference -> {
+            View v = getActivity().getLayoutInflater().inflate(R.layout.dialog_font_size, null);
+
+            assert v != null;
+            final SeekBar seekBar = (SeekBar) v.findViewById(R.id.value_seekbar);
+            seekBar.setProgress(Preferences.Main.getWebViewSize() - 1 - 7);
+            final TextView textView = (TextView) v.findViewById(R.id.value_textview);
+            textView.setText(Integer.toString(seekBar.getProgress() + 1 + 7));
+            textView.setTextSize(seekBar.getProgress() + 1 + 7);
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    textView.setText(Integer.toString(i + 1 + 7));
+                    textView.setTextSize(i + 1 + 7);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+            AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                    .setTitle("Размер шрифта")
+                    .setView(v)
+                    .setPositiveButton("Ок", (dialog1, which) -> Preferences.Main.setWebViewSize(seekBar.getProgress() + 1 + 7))
+                    .setNegativeButton("Отмена", null)
+                    .setNeutralButton("Сброс", null)
+                    .show();
+            dialog.getButton(DialogInterface.BUTTON_NEUTRAL).setOnClickListener(v1 -> {
+                seekBar.setProgress(16 - 1 - 7);
+                Preferences.Main.setWebViewSize(16);
+                App.getInstance().getPreferences().edit().putInt(Preferences.Main.WEBVIEW_FONT_SIZE, 16).apply();
+            });
+            return false;
+        });
     }
 }
