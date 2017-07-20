1eb3c8d10f1c4092bda67cf5562f3d5b640c1119
Aleksandr <neizvestniytip@gmail.com>
2017-07-20 23:35:58 +0400

    просто пиздец
    Former-commit-id: 73a3560c03d76635795d65de1c6142cd09c2fded

--- app/src/main/java/forpdateam/ru/forpda/api/NewsItem.java
+++ app/src/main/java/forpdateam/ru/forpda/api/NewsItem.java
@@ -3,7 +3,7 @@ package forpdateam.ru.forpda.api;
 /**
  * Created by isanechek on 7/20/17.
  */
-
+// На время только, ибо оригинал в котлине
 public class NewsItem {
     public String url;
     public String title;
