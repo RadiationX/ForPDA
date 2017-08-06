f64b0ed6b40131a90553c0061a651492c4ba777f
RadiationX <radiationx@yandex.ru>
2017-08-06 14:47:43 +0300

    new devdb api
    Former-commit-id: 5aaa5145b468dbebc8180b820610501d64963b53

---
+++ app/src/main/java/forpdateam/ru/forpda/api/ndevdb/models/Device.java
@@ -0,0 +1,85 @@
+package forpdateam.ru.forpda.api.ndevdb.models;
+
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+/**
+ * Created by radiationx on 06.08.17.
+ */
+
+public class Device {
+    public final static Pattern PATTERN_1 = Pattern.compile("h1 class=\"product-name\">(?:<a[^>]*?>[^<]*?<\\/a>)? ?([^<]*?)<\\/h1>[\\s\\S]*?div class=\"item-visual\">([\\s\\S]*?)<\\/div>[^<]*?<div class=\"item-info\">[\\s\\S]*?div class=\"item-content[^>]*?>[^<]*?<div class=\"content\">([\\s\\S]*?)<\\/div>[^<]*?<div class=\"aside\">");
+    public final static Pattern IMAGES_PATTERN = Pattern.compile("<a[^>]*?href=\"([^\"]*?)\"[^>]*?><img src=\"([^\"]*?)\"");
+    public final static Pattern SPECS_TITLED_PATTERN = Pattern.compile("<div class=\"specifications-list\"><h3[^>]*?>([^>]*?)<\\/h3>([\\s\\S]*?)<\\/div>");
+
+    private ArrayList<Pair<String, ArrayList<Pair<String, String>>>> specs = new ArrayList<>();
+    private ArrayList<Pair<String, String>> images = new ArrayList<>();
+    private Pair<Integer, String> rating;
+    private String id;
+    private String title;
+    private String manId;
+    private String manTitle;
+    private String catId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getManId() {
+        return manId;
+    }
+
+    public void setManId(String manId) {
+        this.manId = manId;
+    }
+
+    public String getManTitle() {
+        return manTitle;
+    }
+
+    public void setManTitle(String manTitle) {
+        this.manTitle = manTitle;
+    }
+
+    public String getCatId() {
+        return catId;
+    }
+
+    public void setCatId(String catId) {
+        this.catId = catId;
+    }
+
+    public void addImage(String imageSrc, String fullImageSrc) {
+        images.add(new Pair<>(imageSrc, fullImageSrc));
+    }
+
+    public ArrayList<Pair<String, String>> getImages() {
+        return images;
+    }
+
+    public void setRating(int num, String text) {
+        this.rating = new Pair<>(num, text);
+    }
+
+    public Pair<Integer, String> getRating() {
+        return rating;
+    }
+
+    public void addSpecs(String title, ArrayList<Pair<String, String>> specs) {
+        this.specs.add(new Pair<>(title, specs));
+    }
+}
---
+++ app/src/main/java/forpdateam/ru/forpda/api/ndevdb/models/Manufacturer.java
@@ -0,0 +1,127 @@
+package forpdateam.ru.forpda.api.ndevdb.models;
+
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+/**
+ * Created by radiationx on 06.08.17.
+ */
+
+public class Manufacturer {
+    public final static Pattern DEVICES_PATTERN = Pattern.compile("<div class=\"box-holder\">[^<]*?<div class=\"visual\">[^<]*?<a[^>]*?>[^<]*?<span[^>]*?><img src=\"([^\"]*?)\"[^>]*?>[\\s\\S]*?<div class=\"name\"><a href=\"[^\"]*?devdb\\/([^\"]*?)\"[^>]*?>([\\s\\S]*?)<\\/a>[\\s\\S]*?<div class=\"specifications-list\">([\\s\\S]*?)<\\/div>(?:<div class=\"price\">[^<]*?<strong>([^<]*?)<\\/strong>)?[\\s\\S]*?<div class=\"rating-col\">(?:[^<]*?<div class=\"rating r\\d\">[^<]*?<div class=\"num\">(\\d+)<\\/div>[^<]*?<div class=\"text\">([\\s\\S]*?)<\\/div>[^<]*?<\\/div>)?");
+
+    private ArrayList<DeviceItem> devices = new ArrayList<>();
+    private String id;
+    private String title;
+    private String catId;
+
+    private int actual = 0;
+    private int all = 0;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCatId() {
+        return catId;
+    }
+
+    public void setCatId(String catId) {
+        this.catId = catId;
+    }
+
+    public int getActual() {
+        return actual;
+    }
+
+    public void setActual(int actual) {
+        this.actual = actual;
+    }
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+
+    public void addDevice(DeviceItem item) {
+        devices.add(item);
+    }
+
+    public ArrayList<DeviceItem> getDevices() {
+        return devices;
+    }
+
+    public static class DeviceItem {
+        private ArrayList<Pair<String, String>> specs = new ArrayList<>();
+        private Pair<Integer, String> rating;
+        private String id;
+        private String title;
+        private String price;
+        private String imageSrc;
+
+        public String getImageSrc() {
+            return imageSrc;
+        }
+
+        public void setImageSrc(String imageSrc) {
+            this.imageSrc = imageSrc;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getPrice() {
+            return price;
+        }
+
+        public void setPrice(String price) {
+            this.price = price;
+        }
+
+        public Pair<Integer, String> getRating() {
+            return rating;
+        }
+
+        public void setRating(int num, String text) {
+            this.rating = new Pair<>(num, text);
+        }
+
+        public void addSpec(String a1, String value) {
+            specs.add(new Pair<>(a1, value));
+        }
+
+        public ArrayList<Pair<String, String>> getSpecs() {
+            return specs;
+        }
+    }
+}
