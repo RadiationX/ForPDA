0ae3a6d9f77e7c788eda8e6eb33566630bd8b5b8
RadiationX <radiationx@yandex.ru>
2017-08-09 23:49:15 +0300

    add devdb posts ui
    Former-commit-id: 473b018e960ec78d123a2dea347481dfa23555bb

--- app/src/main/java/forpdateam/ru/forpda/fragments/devdb/device/SubDeviceFragment.java
+++ app/src/main/java/forpdateam/ru/forpda/fragments/devdb/device/SubDeviceFragment.java
@@ -1,5 +1,7 @@
 package forpdateam.ru.forpda.fragments.devdb.device;
 
+import android.support.v4.app.Fragment;
+
 import forpdateam.ru.forpda.api.ndevdb.models.Device;
 import forpdateam.ru.forpda.fragments.TabFragment;
 
@@ -7,7 +9,7 @@ import forpdateam.ru.forpda.fragments.TabFragment;
  * Created by radiationx on 09.08.17.
  */
 
-public class SubDeviceFragment extends TabFragment {
+public class SubDeviceFragment extends Fragment {
     protected Device device;
 
     public Device getDevice() {
