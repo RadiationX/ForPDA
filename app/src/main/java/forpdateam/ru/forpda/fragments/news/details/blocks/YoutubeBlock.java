8518795681450b052ebb46a9a7ec7d236c95f8ab
Aleksandr <neizvestniytip@gmail.com>
2017-08-24 05:07:41 +0400

    Youtube preview Other UI optimization
    Former-commit-id: 6968d0ef27018f85b25186ba6d210ecf992a1c17

--- app/src/main/java/forpdateam/ru/forpda/fragments/news/details/blocks/YoutubeBlock.java
+++ app/src/main/java/forpdateam/ru/forpda/fragments/news/details/blocks/YoutubeBlock.java
@@ -8,14 +8,37 @@ public class YoutubeBlock {
 
     private String title;
     private String id;
+    private String previewImgUrl;
+    private String url;
 
     public YoutubeBlock(String id) {
         this.id = id;
     }
 
-    public YoutubeBlock(String title, String id) {
+    public YoutubeBlock(String id, String url) {
+        this.id = id;
+        this.url = url;
+    }
+
+    public YoutubeBlock(String id, String previewImgUrl, String url) {
+        this.id = id;
+        this.previewImgUrl = previewImgUrl;
+        this.url = url;
+    }
+
+    public YoutubeBlock(String title, String id, String previewImgUrl, String url) {
         this.title = title;
         this.id = id;
+        this.previewImgUrl = previewImgUrl;
+        this.url = url;
+    }
+
+    public String getPreviewImgUrl() {
+        return previewImgUrl;
+    }
+
+    public String getUrl() {
+        return url;
     }
 
     public String getTitle() {
