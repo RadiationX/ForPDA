4f91ccc3965c61704156e3bbe6a81d5bf7ef4b47
RadiationX <radiationx@yandex.ru>
2017-09-04 18:17:56 +0300

    improve send comment
    Former-commit-id: 6b9f4e44e86a258b2fbdfddb2b9d0649074afe75

--- app/src/main/java/forpdateam/ru/forpda/fragments/news/details/ArticleCommentsFragment.java
+++ app/src/main/java/forpdateam/ru/forpda/fragments/news/details/ArticleCommentsFragment.java
@@ -1,16 +1,22 @@
 package forpdateam.ru.forpda.fragments.news.details;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -27,6 +33,7 @@ import forpdateam.ru.forpda.fragments.devdb.BrandFragment;
 import forpdateam.ru.forpda.fragments.theme.ThemeHelper;
 import forpdateam.ru.forpda.rxapi.RxApi;
 import forpdateam.ru.forpda.utils.IntentHandler;
+import forpdateam.ru.forpda.utils.SimpleTextWatcher;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
@@ -39,8 +46,13 @@ import io.reactivex.schedulers.Schedulers;
 
 public class ArticleCommentsFragment extends Fragment implements ArticleCommentsAdapter.ClickListener {
     private RecyclerView recyclerView;
+    private EditText messageField;
+    private FrameLayout sendContainer;
+    private AppCompatImageButton buttonSend;
+    private ProgressBar progressBarSend;
     private DetailsPage article;
-    ArticleCommentsAdapter adapter;
+    private ArticleCommentsAdapter adapter;
+    private Comment currentReplyComment;
 
     public ArticleCommentsFragment setArticle(DetailsPage article) {
         this.article = article;
@@ -50,7 +62,13 @@ public class ArticleCommentsFragment extends Fragment implements ArticleComments
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        recyclerView = new RecyclerView(getContext());
+        View view = inflater.inflate(R.layout.article_comments, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.base_list);
+        messageField = (EditText) view.findViewById(R.id.message_field);
+        sendContainer = (FrameLayout) view.findViewById(R.id.send_container);
+        buttonSend = (AppCompatImageButton) view.findViewById(R.id.button_send);
+        progressBarSend = (ProgressBar) view.findViewById(R.id.send_progress);
+
         recyclerView.setBackgroundColor(App.getColorFromAttr(getContext(), R.attr.background_for_lists));
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
@@ -68,11 +86,21 @@ public class ArticleCommentsFragment extends Fragment implements ArticleComments
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(adapter::addAll);
-
-
         recyclerView.setAdapter(adapter);
 
-        return recyclerView;
+        messageField.addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 0) {
+                    currentReplyComment = null;
+                }
+                buttonSend.setClickable(s.length() > 0);
+            }
+        });
+
+        buttonSend.setOnClickListener(v -> sendComment());
+
+        return view;
     }
 
     @Override
@@ -95,25 +123,37 @@ public class ArticleCommentsFragment extends Fragment implements ArticleComments
 
     @Override
     public void onReplyClick(Comment comment, int position) {
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View layout = inflater.inflate(R.layout.report_layout, null);
+        if (messageField.getText().length() == 0) {
+            currentReplyComment = comment;
+            messageField.setText(currentReplyComment.getUserNick() + ",\n");
+        } else {
+            new AlertDialog.Builder(getContext())
+                    .setMessage("Уже имеется введёный текст, очистить?")
+                    .setPositiveButton("Да", (dialog, which) -> {
+                        currentReplyComment = comment;
+                        messageField.setText(currentReplyComment.getUserNick() + ",\n");
+                    })
+                    .setNegativeButton("Отмена", null)
+                    .show();
+        }
 
-        assert layout != null;
-        final EditText messageField = (EditText) layout.findViewById(R.id.report_text_field);
-
-        new AlertDialog.Builder(getContext())
-                .setTitle("Ответ на коммент ".concat(comment.getUserNick()))
-                .setView(layout)
-                .setPositiveButton("Отправить", (dialogInterface, i) -> {
-                    RxApi.NewsList().replyComment(article, comment.getId(), messageField.getText().toString())
-                            .subscribeOn(Schedulers.io())
-                            .observeOn(AndroidSchedulers.mainThread())
-                            .subscribe(newComments -> {
-                                ArrayList<Comment> comments = Api.NewsApi().commentsToList(article.getCommentTree());
-                                adapter.addAll(comments);
-                            });
-                })
-                .setNegativeButton("Отмена", null)
-                .show();
     }
+
+    private void sendComment() {
+        progressBarSend.setVisibility(View.VISIBLE);
+        buttonSend.setVisibility(View.GONE);
+        int commentId = currentReplyComment == null ? 0 : currentReplyComment.getId();
+        RxApi.NewsList().replyComment(article, commentId, messageField.getText().toString())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(newComments -> {
+                    progressBarSend.setVisibility(View.GONE);
+                    buttonSend.setVisibility(View.VISIBLE);
+                    messageField.setText(null);
+                    currentReplyComment = null;
+                    ArrayList<Comment> comments = Api.NewsApi().commentsToList(article.getCommentTree());
+                    adapter.addAll(comments);
+                });
+    }
+
 }
