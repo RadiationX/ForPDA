044b686426ba627383604501c346baea0ac26afd
RadiationX <radiationx@yandex.ru>
2017-10-16 21:38:00 +0500

    add forum rules
    Former-commit-id: 1cc6d4dd33295e89c27e26ba31f488f936d436fd

--- app/src/main/java/forpdateam/ru/forpda/rxapi/apiclasses/ForumRx.java
+++ app/src/main/java/forpdateam/ru/forpda/rxapi/apiclasses/ForumRx.java
@@ -1,7 +1,10 @@
 package forpdateam.ru.forpda.rxapi.apiclasses;
 
+import biz.source_code.miniTemplator.MiniTemplator;
+import forpdateam.ru.forpda.App;
 import forpdateam.ru.forpda.api.Api;
 import forpdateam.ru.forpda.api.forum.models.ForumItemTree;
+import forpdateam.ru.forpda.api.forum.models.ForumRules;
 import io.reactivex.Observable;
 
 /**
@@ -12,7 +15,31 @@ public class ForumRx {
     public Observable<ForumItemTree> getForums() {
         return Observable.fromCallable(() -> Api.Forum().getForums());
     }
+
     public Observable<Object> markAllRead() {
         return Observable.fromCallable(() -> Api.Forum().markAllRead());
+    }
+
+    public Observable<ForumRules> getRules(boolean withHtml) {
+        return Observable.fromCallable(() -> transform(Api.Forum().getRules(), withHtml));
+    }
+
+    public static ForumRules transform(ForumRules rules, boolean withHtml) throws Exception {
+        if (withHtml) {
+            MiniTemplator t = App.get().getTemplate(App.TEMPLATE_FORUM_RULES);
+            App.setTemplateResStrings(t);
+            t.setVariableOpt("style_type", App.get().getCssStyleType());
+
+            for (ForumRules.Item item : rules.getItems()) {
+                t.setVariable("type", item.isHeader() ? "header" : "");
+                t.setVariable("number", item.getNumber());
+                t.setVariable("text", item.getText());
+                t.addBlock("rules_item");
+            }
+
+            rules.setHtml(t.generateOutput());
+            t.reset();
+        }
+        return rules;
     }
 }
